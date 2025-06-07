212ec41951b88376af5f6b312d6018530a327822
VG <15960525+brillcp@users.noreply.github.com>
2025-06-07 11:28:37 +0200

    Adds Google Books API integration
    Sets up the initial structure for interacting with the
    Google Books API.
    
    Defines API endpoints and request configurations, enabling
    the app to search for books using a query string.

---
+++ Netgear-iOS/GoogleBookAPI/APIEndpoint.swift
@@ -0,0 +1,22 @@
+//
+//  APIEndpoint.swift
+//  Netgear-iOS
+//
+//  Created by Viktor Gidlöf on 2025-06-07.
+//
+
+import Networking
+
+enum APIEndpoint {
+    case search(query: String)
+}
+
+// MARK: - EndpointType
+extension APIEndpoint: EndpointType {
+    var path: String {
+        switch self {
+        case .search(let q):
+            "volumes?q=\(q)"
+        }
+    }
+}
---
+++ Netgear-iOS/GoogleBookAPI/GoogleBookAPI.swift
@@ -0,0 +1,26 @@
+//
+//  GoogleBookAPI.swift
+//  Netgear-iOS
+//
+//  Created by Viktor Gidlöf on 2025-06-07.
+//
+
+import Foundation
+import Networking
+
+enum GoogleBookAPI {
+    case search(query: String)
+}
+
+// MARK: - Requestable
+extension GoogleBookAPI: Requestable {
+    var encoding: Request.Encoding { .query }
+    var httpMethod: HTTP.Method { .get }
+    
+    var endpoint: EndpointType {
+        switch self {
+        case .search(let q):
+            APIEndpoint.search(query: q)
+        }
+    }
+}
